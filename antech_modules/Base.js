88c0a54c922e056079c1e39a543e2912ab243ab6
ENDPANDA <103783569+Endpanda@users.noreply.github.com>
2022-04-16 10:04:48 +0530

    Create Base.js

---
+++ antech_modules/Base.js
@@ -0,0 +1,21 @@
+/*
+Copyright (C) 2021 Endpanda.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+*/
+
+'use strict';
+
+class Base {
+    constructor(client) {
+        Object.defineProperty(this, 'client', { value: client });
+    }
+
+    _clone() {
+        return Object.assign(Object.create(this), this);
+    }
+    
+    _patch(data) { return data; }
+}
+
+module.exports = Base;
